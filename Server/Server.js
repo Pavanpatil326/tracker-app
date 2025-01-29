cccc538baa75c12f99e41dc17acd0975e6a74960
Pavan Patil <sanjayratanpatil9@gmail.com>
2025-01-29 13:47:40 +0530

    first commit

---
+++ Server/Server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan'); 
+const connectDB = require('../db')
+
+// Load environment variables
+dotenv.config({ path: '../config.env' });
+
+// connect To Database
+
+connectDB();
+
+// Import transactions routes
+const transactions = require('../routes/transactions'); // Corrected path if routes is in the same folder as server.js
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // To handle JSON body parsing
+
+if(process.env.NODE_ENV === 'devlopment') {
+  app.use(morgan('dev')); 
+}
+
+// Routes
+app.use('/api/v1/transactions', transactions);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+
+  app.get('*',(req, res) => res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html')));
+}
+
+
+
+// Port setup
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV || 'production'} mode on port ${PORT}`.yellow.bold
+  );
+});
